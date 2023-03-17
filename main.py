e0a162647cfa1999492c7f03981f504841976e2c
fudeel <17783461+fudeel@users.noreply.github.com>
2023-03-17 11:43:56 +0100

    inserted traps

--- main.py
+++ main.py
@@ -23,6 +23,17 @@ for i in range(20):
             board[row][col] = "C"
             break
 
+
+# Put cards in map
+for i in range(12):
+    while True:
+        row = random.randint(0, 29)
+        col = random.randint(0, 29)
+        if board[row][col] == 0 and "X" not in board[row] and "X" not in [board[r][col] for r in range(30)] \
+                and "C" not in board[row] and "C" not in [board[r][col] for r in range(30)]:
+            board[row][col] = "T"
+            break
+
 goku = Character('Goku', 3, 4, 15, 10, False, 2, 20, 20)
 freezer = Character('Freezer', 5, 5, 15, 10, False, 4, 20, 4)
 
