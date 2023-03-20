3efbc63768d527231f5221cc6de596a0ac7c54b0
fudeel <17783461+fudeel@users.noreply.github.com>
2023-03-20 15:48:30 +0100

    refactor

--- models/card.py
+++ models/card.py
@@ -1,9 +1,10 @@
 class Card:
 
-    def __init__(self, id, desc, tar):
+    def __init__(self, id, desc, tar, eff_code):
         self._id = id  # card id
         self._description = desc  # card description
         self._target = tar  # card effect's target me | other | cells | all
+        self._effect_code = eff_code  # card effect code
 
     def get_id(self):
         return self._id
@@ -22,3 +23,9 @@ class Card:
 
     def set_target(self, tar):
         self._target = tar
+
+    def get_effect_code(self):
+        return self._effect_code
+
+    def set_effect_code(self, code):
+        self._effect_code = code
---
+++ utils/cards.py
@@ -0,0 +1,7 @@
+from models.card import Card
+
+card_1 = Card(
+    id=1,
+    tar='me',
+    desc='Increase attack power by 2'
+)
