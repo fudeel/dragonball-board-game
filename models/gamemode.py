157164bc4b79d45fa0a3eff24d2a08fa0df4c59d
fudeel <17783461+fudeel@users.noreply.github.com>
2023-03-20 13:46:25 +0100

    created gamemode object to make board dynamic

---
+++ models/gamemode.py
@@ -0,0 +1,7 @@
+class GameMode:
+    def __init__(self, id, size, s, c, t):
+        self.id = id
+        self.size = size
+        self.sphers = s
+        self.cards = c
+        self.traps = t
--- set/board.py
+++ set/board.py
@@ -1,8 +1,32 @@
 import random
 
+from models.gamemode import GameMode
 from utils.game import game_state, save_game
 
-board = [[0 for j in range(16)] for i in range(16)]
+"""gamemodes that generate different map
+
+[1] 16x16 / 7 spheres / 20 traps / 20 cards
+
+"""
+
+
+gm = GameMode(id=1, size=16, s=7, c=5, t=5)
+
+print(f"{gm.size}")
+board = [[0 for j in range(gm.size)] for i in range(gm.size)]
+
+
+def ensure_numbers_of_elements(gb):
+    char_count = {'C': 0, 'T': 0, 'X': 0}
+
+    for row in gb:
+        for char in row:
+            if char in char_count:
+                char_count[char] += 1
+
+    print(f"Number of 'C's: {char_count['C']}")
+    print(f"Number of 'T's: {char_count['T']}")
+    print(f"Number of 'X's: {char_count['X']}")
 
 
 def generate_game_board():
@@ -10,36 +34,39 @@ def generate_game_board():
     game_board = board
 
     # Put balls in map
-    for i in range(5):
+    for i in range(gm.sphers):
         while True:
-            row = random.randint(0, 15)
-            col = random.randint(0, 15)
+            row = random.randint(0, gm.size - 1)
+            col = random.randint(0, gm.size - 1)
             if game_board[row][col] == 0:
                 game_board[row][col] = "X"
                 break
 
     # Put cards in map
-    for i in range(10):
+    for i in range(gm.cards):
         while True:
-            row = random.randint(0, 15)
-            col = random.randint(0, 15)
+            row = random.randint(0, gm.size - 1)
+            col = random.randint(0, gm.size - 1)
             if game_board[row][col] == 0 and "X" not in game_board[row] \
-                    and "X" not in [game_board[r][col] for r in range(16)]:
+                    and "X" not in [game_board[r][col] for r in range(gm.size - 1)]:
                 game_board[row][col] = "C"
                 break
 
     # Put traps in map
-    for i in range(10):
+    for i in range(gm.traps):
         while True:
-            row = random.randint(0, 15)
-            col = random.randint(0, 15)
+            row = random.randint(0, gm.size - 1)
+            col = random.randint(0, gm.size - 1)
             if game_board[row][col] == 0 and "X" not in game_board[row] \
-                    and "X" not in [game_board[r][col] for r in range(16)] \
-                    and "C" not in game_board[row] and "C" not in [game_board[r][col] for r in range(16)]:
+                    and "X" not in [game_board[r][col] for r in range(gm.size - 1)] \
+                    and "C" not in game_board[row] and "C" not in [game_board[r][col] for r in range(gm.size - 1)]:
                 game_board[row][col] = "T"
                 break
+
+    # ensure_numbers_of_elements(game_board)
 
     game_state['board'] = game_board
     save_game(game_state, 'saved_game.json')
 
     return game_board
+
